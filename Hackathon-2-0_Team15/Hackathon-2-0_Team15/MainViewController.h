48c68ec2c87dbcfa940e9e83b4336c9ec576851c
Jordan <j.homan@me.com>
2012-08-25 09:07:51 -0400

    Added main view controller class.

---
+++ Hackathon-2-0_Team15/Hackathon-2-0_Team15/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Hackathon-2-0_Team15
+//
+//  Created by Jordan Homan on 8/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
